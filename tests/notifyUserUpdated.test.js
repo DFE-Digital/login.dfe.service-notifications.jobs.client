dbbdcdf4374ab4d61eb8660e9c6c0d38c390f687
Simon Bull <simon.bull.84@googlemail.com>
2018-12-11 15:19:26 +0000

    Add notify user updated method

---
+++ tests/notifyUserUpdated.test.js
@@ -0,0 +1,119 @@
+jest.mock('kue');
+
+const omit = require('lodash/omit');
+const kue = require('kue');
+const PublicApiClient = require('./../lib');
+
+const job = {
+  save: jest.fn(),
+};
+const queue = {
+  create: jest.fn(),
+};
+const connectionString = 'some-redis-connection-string';
+const user = {
+  sub: 'user-1',
+  email: 'user.one@unit.test',
+  status: {
+    id: 1,
+  },
+};
+
+describe('when sending userupdated_v1', () => {
+  let client;
+
+  beforeEach(() => {
+    job.save.mockReset().mockImplementation((cb) => {
+      cb();
+    });
+
+    queue.create.mockReset().mockReturnValue(job);
+
+    kue.createQueue.mockReset().mockReturnValue(queue);
+
+    client = new PublicApiClient({ connectionString });
+  });
+
+  it('then it should create new queue connection to redis', async () => {
+    await client.notifyUserUpdated(user);
+
+    expect(kue.createQueue).toHaveBeenCalledTimes(1);
+    expect(kue.createQueue.mock.calls[0][0]).toEqual({
+      redis: connectionString,
+    });
+  });
+
+  it('then it should create job with correct type', async () => {
+    await client.notifyUserUpdated(user);
+
+    expect(queue.create).toHaveBeenCalledTimes(1);
+    expect(queue.create.mock.calls[0][0]).toBe('userupdated_v1');
+  });
+
+  it('then it should create job with correct data', async () => {
+    await client.notifyUserUpdated(user);
+
+    expect(queue.create).toHaveBeenCalledTimes(1);
+    expect(queue.create.mock.calls[0][1]).toEqual(user);
+  });
+
+  it('then it should save job', async () => {
+    await client.notifyUserUpdated(user);
+
+    expect(job.save).toHaveBeenCalledTimes(1);
+  });
+
+  it('then it should error if fails to save job', async () => {
+    job.save.mockImplementation(() => {
+      throw new Error('test');
+    });
+
+    try {
+      await client.notifyUserUpdated(user);
+      throw new Error('no error thrown');
+    } catch (e) {
+      expect(e.message).toBe('test');
+      expect(job.save).toHaveBeenCalledTimes(1);
+    }
+  });
+
+  it('then it should error if user not passed', async () => {
+    try {
+      await client.notifyUserUpdated(undefined);
+      throw new Error('no error thrown');
+    } catch (e) {
+      expect(e.message).toBe('User must be provided');
+      expect(job.save).toHaveBeenCalledTimes(0);
+    }
+  });
+
+  it('then it should error if user has no sub', async () => {
+    try {
+      await client.notifyUserUpdated(omit(user, ['sub']));
+      throw new Error('no error thrown');
+    } catch (e) {
+      expect(e.message).toBe('User must have sub');
+      expect(job.save).toHaveBeenCalledTimes(0);
+    }
+  });
+
+  it('then it should error if user has no email', async () => {
+    try {
+      await client.notifyUserUpdated(omit(user, ['email']));
+      throw new Error('no error thrown');
+    } catch (e) {
+      expect(e.message).toBe('User must have email');
+      expect(job.save).toHaveBeenCalledTimes(0);
+    }
+  });
+
+  it('then it should error if user has no status.id', async () => {
+    try {
+      await client.notifyUserUpdated(omit(user, ['status']));
+      throw new Error('no error thrown');
+    } catch (e) {
+      expect(e.message).toBe('User must have status.id');
+      expect(job.save).toHaveBeenCalledTimes(0);
+    }
+  });
+});
